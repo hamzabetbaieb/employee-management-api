ddb3ef17baf70edbf50aa8c639f027e74551f167
Hamza Betbaieb <hamza.betbaieb@esprit.tn>
2026-02-27 04:56:26 +0100

    CRUD department

--- src/main/java/com/example/employee_api/service/EmployeeService.java
+++ src/main/java/com/example/employee_api/service/EmployeeService.java
@@ -1,7 +1,9 @@
 package com.example.employee_api.service;
 
 import com.example.employee_api.dto.EmployeeDto;
+import com.example.employee_api.entity.Department;
 import com.example.employee_api.entity.Employee;
+import com.example.employee_api.repository.DepartmentRepository;
 import com.example.employee_api.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,8 @@ public class EmployeeService {
 
     @Autowired
     private EmployeeRepository employeerepository;
+    @Autowired
+    private DepartmentRepository departmentRepository;
 
     public List<Employee> getAllEmployees(){
         List<Employee>employees = employeerepository.findAll();
@@ -30,23 +34,27 @@ public Employee createEmployee(EmployeeDto e){
 
     if(employeerepository.findByEmail(e.getEmail()) != null) {
         throw new RuntimeException("Email already exists");}
+    Department department = departmentRepository.findById(e.getDepartmentId()).orElseThrow(()->new RuntimeException("department not found"));
 
     Employee employee = new Employee();
     employee.setName(e.getName());
     employee.setEmail(e.getEmail());
     employee.setSalary(e.getSalary());
+    employee.setDepartment(department);
     return employeerepository.save(employee);
-
 }
 public Employee updateEmployee(Long id, EmployeeDto e){
     Employee existing = employeerepository.findByEmail(e.getEmail());
     if(existing != null && !existing.getId().equals(id)) {
         throw new RuntimeException("Email already exists");
     }
+    Department department = departmentRepository.findById(e.getDepartmentId()).orElseThrow(()->new RuntimeException("department not found"));
+
     Employee employee =employeerepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
         employee.setName(e.getName());
         employee.setEmail(e.getEmail());
         employee.setSalary(e.getSalary());
+        employee.setDepartment(department);
         return employeerepository.save(employee);
 }
 
@@ -55,7 +63,7 @@ public void deleteEmployee(Long id){
       employeerepository.delete(employee);
       System.out.println("l'employé dont id "+ id + "est supprimé ");
 }
-    public Employee searchEmployeeByEmail(String email){
+public Employee searchEmployeeByEmail(String email){
         Employee employee = employeerepository.findByEmail(email);
         if(employee == null){
             throw new RuntimeException("Employee not found with email: " + email);
